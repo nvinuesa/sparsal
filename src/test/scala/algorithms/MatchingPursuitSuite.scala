677f4d4e0cd435f1da6cca51ad9a30bd5a1d6687
underscorenico <vinuesa.nico@gmail.com>
2017-01-25 21:53:54 +0100

    feat: add sine test

--- src/test/scala/algorithms/MatchingPursuitSuite.scala
+++ src/test/scala/algorithms/MatchingPursuitSuite.scala
@@ -17,4 +17,23 @@ class MatchingPursuitSuite extends FunSuite {
     assert(b._2.length == 10)
     assert(snr.estimate(subtraction(orig, b._2), orig))
   }
+
+  test("Sparse decomposition of sine wave") {
+    val orig: Seq[Double] = sine(1000, 1, 48000, 480)
+    val dict = new Gabor(orig)
+    val a = new MatchingPursuit[Double](orig, dict)
+    val snr = new SNR[Double](4.0)
+    val b = a.run(snr)
+
+    assert(b._2.length == orig.length)
+    assert(snr.estimate(subtraction(orig, b._2), orig))
+  }
+
+  def sine(f: Double, amp: Double, samplef: Double, samples: Int): IndexedSeq[Double] = {
+    val wavePeriod = 1 / f
+    val samplePeriod = 1 / samplef
+    val dTheta = (samplePeriod / wavePeriod) * 2 * math.Pi
+
+    (0 until samples).map(n => math.sin(dTheta * n) * amp)
+  }
 }
